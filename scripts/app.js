2ab2e2763d9e3c9aa1274eda6e245a883c91f824
bilalkeskinosf <bilal.keskin@osf.digital>
2023-01-29 09:39:21 +0300

    finished product v1

---
+++ scripts/app.js
@@ -0,0 +1,77 @@
+let slideIndex = 1;
+const slidesToShow = 3;
+const prevButton = document.querySelector('.prev');
+const nextButton = document.querySelector('.next');
+let touchstartX = 0;
+let touchstartY = 0;
+let touchendX = 0;
+let touchendY = 0;
+
+showSlides(slideIndex);
+
+function plusSlides(n) {
+	showSlides((slideIndex += n));
+}
+
+function showSlides(n) {
+	let i;
+	let slides = document.getElementsByClassName('cart--items');
+	if (n > slides.length - 2) {
+		slideIndex = 1;
+	}
+	if (n < 1) {
+		slideIndex = slides.length - 2;
+	}
+	for (i = 0; i < slides.length; i++) {
+		slides[i].style.display = 'none';
+	}
+	for (i = 0; i < slidesToShow; i++) {
+		slides[slideIndex - 1 + i].style.display = 'block';
+	}
+	if (slideIndex + 2 > slides.length - 1) {
+		nextButton.style.display = 'none';
+	} else {
+		nextButton.style.display = 'block';
+	}
+	if (slideIndex == 1) {
+		prevButton.style.display = 'none';
+	} else {
+		prevButton.style.display = 'block';
+	}
+}
+
+prevButton.addEventListener('click', () => {
+	plusSlides(-1);
+});
+
+nextButton.addEventListener('click', () => {
+	plusSlides(1);
+});
+
+document.addEventListener(
+	'touchstart',
+	function (event) {
+		touchstartX = event.touches[0].clientX;
+		touchstartY = event.touches[0].clientY;
+	},
+	false
+);
+
+document.addEventListener(
+	'touchend',
+	function (event) {
+		touchendX = event.changedTouches[0].clientX;
+		touchendY = event.changedTouches[0].clientY;
+		handleGesture();
+	},
+	false
+);
+
+function handleGesture() {
+	if (touchendX <= touchstartX) {
+		plusSlides(1);
+	}
+	if (touchendX >= touchstartX) {
+		plusSlides(-1);
+	}
+}
